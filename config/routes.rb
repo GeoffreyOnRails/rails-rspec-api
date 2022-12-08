c0911aa0612abe21efa578c50104d652666ed703
Amine Lalaoui <amine_lalaoui@hotmail.com>
2022-12-08 17:03:53 +0100

    Switch to postgresql

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  namespace :potato_ton_snapshot do
-    get "/given_day", to: "potato_ton_snapshot#given_day"
-  end
+  get "/potato_ton_snapshot/given_day", to: "potato_ton_snapshot#given_day"
+  get "/potato_ton_snapshot/best_day_trade", to: "potato_ton_snapshot#best_day_trade"
 end
